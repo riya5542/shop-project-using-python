ad91f1881818553d6f26b840f13bc7a256037502
Riya Mishra <129922354+riya5542@users.noreply.github.com>
2023-09-20 11:52:48 +0530

    Add files via upload

---
+++ mall.py
@@ -0,0 +1,469 @@
+print("                        Wel come in City Mall                                          ")
+print("Press 1 for Male Category")
+print("Press 2 for Female Category")
+print("Press 3 for Kids Category")
+a=int(input('Enter your choice '))
+if a==1:
+    print(" Wel come in Male Category")
+    print("Press 1 for shrits")
+    print("press 2 for jens")
+    print("press 3 for lowers")
+    print("press 4 for T-shrits")
+    b=int(input("enter your choice"))
+    if b==1:
+        print(" welcome to shrits category")
+        print("press 1 for long shrits")
+        print("press 2 for short shrits")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("long shrits")
+            d=400
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print(" amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+        if c==2:
+            print("short shrits")
+            d=300
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+    if b==2:
+        print(" welcome to jens category")
+        print("press 1 for color jens")
+        print("press 2 for print jens")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("color jens")
+            d=800
+            print("price of one jens",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+        if c==2:
+            print("print jens")
+            d=900
+            print("price of one jens ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+    if b==3:
+        print(" welcome to lower category")
+        print("press 1 for plan lower")
+        print("press 2 for print lower")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("plan lower")
+            d=300
+            print("price of one lower ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+             
+            
+        if c==2:
+            print("print lower")
+            d=500
+            print("price of one lower ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+             
+    if b==4:
+        print(" welcome to T-Shrits category")
+        print("press 1 for plan T-shrits")
+        print("press 2 for print T-shrits")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("plan T-shrits")
+            d=300
+            print("price of one T-shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+             
+            
+        if c==2:
+            print("print T-shrits")
+            d=500
+            print("price of one T-shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+             
+        
+            
+    
+    
+if a==2:
+    print(" Wel come in FeMale Category")
+    
+    print("Press 1 for Top")
+    print("press 2 for jens")
+    print("press 3 for lowers")
+    print("press 4 for Kruta")
+    b=int(input("enter your choice"))
+    if b==1:
+        print(" welcome to Top category")
+        print("press 1 for short Top")
+        print("press 2 for long Top")
+        c=int(input("enter your choice"))
+        if c==1:
+            print(" short Top")
+            d=300
+            print("price of one Top ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+        if c==2:
+            print(" long Top")
+            d=500
+            print("price of one Top ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+    if b==2:
+        print(" welcome to jens category")
+        print("press 1 for color jens")
+        print("press 2 for print jens")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("color jens")
+            d=900
+            print("price of one jens ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+             
+            
+        if c==2:
+            print("print jens")
+            d=300
+            print("price of one jens ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+    if b==3:
+        print(" welcome to lower category")
+        print("press 1 for plan lower")
+        print("press 2 for color lower")
+        c=int(input("enter your choice"))
+        if c==1:
+            print(" plan lower")
+            d=300
+            print("price of one lower",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+             
+        if c==2:
+            print("print color lower")
+            print("Press 1 for red color")
+            print("Press 2 for blue color")
+            print("press 3 for green color")
+            d=int(input("enter a choice of color"))
+            if d==1:
+                  print("for red color lower")
+                  o=300
+                  print("price of one lower ",o)
+                  e=int(input("enter a quantity"))
+                  f=e*o
+                  print("Total amount",f)
+                  g=0.05
+                  print("GST apply on product", g)
+                  n=f+(5/100)*f
+                  print("Total amount",n)
+            
+            
+             
+            if d==2:
+                  print("for blue color lower")
+                  o=300
+                  print("price of one lower ",o)
+                  e=int(input("enter a quantity"))
+                  f=e*o
+                  print("Total amount",f)
+                  g=0.05
+                  print("GST apply on product", g)
+                  n=f+(5/100)*f
+                  print("Total amount",n)
+            
+            
+            
+            if d==3:
+                  print("for green color lower")
+                  o=300
+                  print("price of one lower ",o)
+                  e=int(input("enter a quantity"))
+                  f=e*o
+                  print("Total amount",f)
+                  g=0.05
+                  print("GST apply on product", g)
+                  n=f+(5/100)*f
+                  print("Total amount",n)
+            
+            
+            
+                  
+                  
+    if b==4:
+        print(" welcome to kruta category")
+        print("press 1 for short kruta")
+        print("press 2 for long kruta")
+        c=int(input("enter your choice"))
+        if c==1:
+            print(" short kruta")
+            d=300
+            print("price of one kruta ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+             
+            
+        if c==2:
+            print(" long kruta")
+            d=300
+            print("price of one kruta",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+             
+            
+        
+    
+
+
+if a==3:
+    print(" Wel come in Kids Category")
+    print("Press 1 for shorts")
+    print("Press 2 for shrit")
+    print("Press 3 for jens")
+    b=int(input("enter your choice"))
+    if b==1:
+        print(" welcome to shorts category")
+        print("press 1 for  plan shorts")
+        print("press 2 for long shorts")
+        c=int(input("enter your choice"))
+        if c==1:
+            print("  plan shorts ")
+            d=300
+            print("price of one shorts ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+        if c==2:
+            print(" long shorts")
+            d=300
+            print("price of one shorts ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+    if b==2:
+        print(" welcome to shrits category")
+        print("press 1 for short shrits")
+        print("press 2 for long shrits")
+        c=int(input("enter your choice"))
+        if c==1:
+            print(" short shrits")
+            d=300
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+             
+        if c==2:
+            print(" long shrits")
+            d=300
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+             
+    if b==3:
+        print(" welcome to jens category")
+        print("press 1 for plan jens")
+        if c==1:
+            print(" plan jens")
+            d=300
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+            
+        if c==2:
+            print(" color jens")
+            d=300
+            print("price of one shrits ",d)
+            e=int(input("enter a quantity"))
+            f=e*d
+            print("Total amount",f)
+            g=0.05
+            print("GST apply on product", g)
+            n=f+(5/100)*f
+            print("Total amount",n)
+            
+            
+            
+                 
+                 
+        
+    
+    
+        
+    
+
+
+
+
+              
+              
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
